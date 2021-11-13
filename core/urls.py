390efd7ffa13112acf7cddd1efdd11ec25a2b4a4
Ismael <ismael@ismael.com>
2021-11-13 20:06:06 -0300

    Incluido funcao de autenticacao

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import lista_eventos
 from django.views.generic import RedirectView
+from .views import lista_eventos, login_user, submit_login, logout_user
+
 
 urlpatterns = [
     path('agenda/', lista_eventos, name='agenda'),
     path('', RedirectView.as_view(url='agenda/')),
+    path('login/', login_user),
+    path('login/submit', submit_login),
+    path('logout/', logout_user),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Evento
 
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario ou senha invalido")
+
+    return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
-    eventos = Evento.objects.all()
+    usuario = request.user
+    eventos = Evento.objects.filter(usuario=usuario)
     ctx = {"eventos": eventos}
     return render(request, 'agenda.html', ctx)
